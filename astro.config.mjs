da725faacdf72eed5e0caac748f4903a683cd499
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-01-12 21:43:34 +0000

    chore: :package: add astro/image
    added astro images package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import mdx from '@astrojs/mdx';
-import { defineConfig } from 'astro/config';
 
+import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
