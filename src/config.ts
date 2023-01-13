cb10911272b1b66f04486f3271418412d7687452
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-01-13 21:12:07 +0000

    chore: :mag: meta
    added site details

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'What i Use';
+export const SITE_DESCRIPTION = 'A list of tools i use and some workflow for coding';
+export const HOMEPAGE_URL = 'https://what-i-use.vercel.app';
