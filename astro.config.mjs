9e317a90be84c3b1fd9df54b284b532708abeb67
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-01-13 21:58:11 +0000

    build: :package: add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
 export default defineConfig({
-site: 'https://what-i-use.vercel.app', //https://uses.wiztek.com
-  integrations: [sitemap(), image(), react(), tailwind()]
+  site: 'https://what-i-use.vercel.app',
+  //https://uses.wiztek.com
+  integrations: [sitemap(), image(), react(), tailwind()],
+  output: "server",
+  adapter: cloudflare()
 });
